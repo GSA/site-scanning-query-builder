98021e7aeabee5149b328c4e32462c1fdf7c3095
Gray Brooks <gbinal@users.noreply.github.com>
2021-03-19 14:39:48 -0400

    fixing commenting out

--- src/components/modules/builder-actions.js
+++ src/components/modules/builder-actions.js
@@ -89,8 +89,8 @@ export const BuilderActions = (props) => {
                 <h2>Choose a template</h2>
                 <a href={LINKS.GOOGLE_SHEETS_LINK} target="_blank" className='usa-link'>
                     Pull data into Google Sheets
-                /*</a>
-                <a href={LINKS.EXCEL_LINK} target="_blank" className='usa-link margin-left-2'>
+                </a>
+                /*<a href={LINKS.EXCEL_LINK} target="_blank" className='usa-link margin-left-2'>
                     Pull data into Microsoft Excel
                 </a>*/
             </div>
